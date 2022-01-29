312dd366a313a956f3e85b3ef82fba9d72dfa63b
Daniel Lins <dlins300@gmail.com>
2022-01-29 17:09:33 -0300

    Commit inicial ( já importado styled components) e feito algumas configurações básicas

---
+++ src/App.styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div `
+    background-color: #24282F;
+    min-height: 100vh;
+    color: #FFF;
+`;
+
+export const Map = styled.div `
+`
